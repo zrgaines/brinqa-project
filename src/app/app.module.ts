91164b6147aca0019cfc0ed8113a7d5e3fae02bd
Zach Gaines <zrgaines.dev@gmail.com>
2017-07-07 15:24:11 -0500

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdTabsModule, MdInputModule } from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MdTabsModule,
+    MdInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
